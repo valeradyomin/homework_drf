6b2d3a91ec20cfb8a2dba493190926c9ac2e3b4e
Valera Demin <wahrwolffe@gmail.com>
2024-01-24 18:12:21 +0300

    add Course and Lesson models

--- materials/models.py
+++ materials/models.py
@@ -1,3 +1,36 @@
 from django.db import models
 
 # Create your models here.
+
+NULLABLE = {
+    'null': True,
+    'blank': True,
+}
+
+
+class Course(models.Model):
+    title = models.CharField(max_length=200, verbose_name='название')
+    image = models.ImageField(upload_to='materials/course/', verbose_name='картинка', **NULLABLE)
+    description = models.TextField(verbose_name='описание', **NULLABLE)
+
+    def __str__(self):
+        return f'{self.title}'
+
+    class Meta:
+        verbose_name = 'курс'
+        verbose_name_plural = 'курсы'
+
+
+class Lesson(models.Model):
+    course = models.ForeignKey(Course, on_delete=models.CASCADE, verbose_name='курс')
+    title = models.CharField(max_length=200, verbose_name='название')
+    description = models.TextField(verbose_name='описание', **NULLABLE)
+    image = models.ImageField(upload_to='materials/lesson/', verbose_name='картинка', **NULLABLE)
+    url_video = models.URLField(verbose_name='ссылка на видео', **NULLABLE)
+
+    def __str__(self):
+        return f'{self.title}'
+
+    class Meta:
+        verbose_name = 'урок'
+        verbose_name_plural = 'уроки'
