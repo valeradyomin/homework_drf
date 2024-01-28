02760793e94b7cf90b37c2aa2ed4d50cd4ec6c54
Valera Demin <wahrwolffe@gmail.com>
2024-01-28 22:18:27 +0300

    HW2: add get_count_lessons

--- materials/serializers.py
+++ materials/serializers.py
@@ -1,9 +1,15 @@
 from rest_framework import serializers
+from rest_framework.fields import SerializerMethodField
 
 from materials.models import Course, Lesson
 
 
 class CourseSerializer(serializers.ModelSerializer):
+    count_lessons = SerializerMethodField()
+
+    def get_count_lessons(self, obj):
+        return Lesson.objects.filter(course=obj).count()
+
     class Meta:
         model = Course
         fields = '__all__'
