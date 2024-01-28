c207cfe030316856c8604c439c41faeef54161cb
Valera <wahrwolffe@gmail.com>
2024-01-29 01:09:11 +0300

    HW2: add UserViewSet

--- users/views.py
+++ users/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+
+from users.models import User
+from users.serializers import UserSerializer
+
 
 # Create your views here.
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
