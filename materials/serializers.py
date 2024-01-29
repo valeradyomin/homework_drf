f8d579c013f946e039d28992cdc3ed7991a5bf6c
Valera Demin <wahrwolffe@gmail.com>
2024-01-29 15:16:02 +0300

    HW2: CourseSerializer upd

--- materials/serializers.py
+++ materials/serializers.py
@@ -4,18 +4,19 @@ from rest_framework.fields import SerializerMethodField
 from materials.models import Course, Lesson
 
 
+class LessonSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Lesson
+        fields = '__all__'
+
+
 class CourseSerializer(serializers.ModelSerializer):
     count_lessons = SerializerMethodField()
+    lessons = LessonSerializer(source='lesson_set', many=True, read_only=True)
 
     def get_count_lessons(self, obj):
         return Lesson.objects.filter(course=obj).count()
 
     class Meta:
         model = Course
-        fields = '__all__'
-
-
-class LessonSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Lesson
         fields = '__all__'
