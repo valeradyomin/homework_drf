467cc4a9bebe1c676d495dfdc9427605e687bb26
Valera Demin <wahrwolffe@gmail.com>
2024-02-10 21:59:35 +0300

    HW3: add create_moderator

--- users/admin.py
+++ users/admin.py
@@ -4,7 +4,7 @@ from users.models import User, Payment
 
 @admin.register(User)
 class AdminUser(admin.ModelAdmin):
-    list_display = ('first_name', 'last_name', 'email', 'phone', 'avatar',)
+    list_display = ('first_name', 'last_name', 'email', 'phone', 'avatar', 'role',)
 
 
 @admin.register(Payment)
