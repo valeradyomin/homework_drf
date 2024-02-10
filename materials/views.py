d11de4b4bd86f66908b4c75bc099b39f164eb66e
Valera Demin <wahrwolffe@gmail.com>
2024-02-11 00:10:31 +0300

    HW3: add create_user

--- materials/views.py
+++ materials/views.py
@@ -1,7 +1,8 @@
 from rest_framework import viewsets, generics
+from rest_framework.permissions import IsAuthenticated
 
 from materials.models import Course, Lesson
-from materials.permissions import IsModerator
+from materials.permissions import IsModerator, IsOwner
 from materials.serializers import CourseSerializer, LessonSerializer
 
 
@@ -12,27 +13,36 @@ class CourseViewSet(viewsets.ModelViewSet):
     serializer_class = CourseSerializer
     queryset = Course.objects.all()
 
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
 
 class LessonCreateAPIView(generics.CreateAPIView):
     serializer_class = LessonSerializer
+    permission_classes = [IsAuthenticated]
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
 
 
 class LessonListAPIView(generics.ListAPIView):
     serializer_class = LessonSerializer
     queryset = Lesson.objects.all()
+    permission_classes = [IsAuthenticated, IsModerator | IsOwner]
 
 
 class LessonRetrieveAPIView(generics.RetrieveAPIView):
     serializer_class = LessonSerializer
     queryset = Lesson.objects.all()
-    permission_classes = [IsModerator]
+    permission_classes = [IsAuthenticated, IsModerator | IsOwner]
 
 
 class LessonUpdateAPIView(generics.UpdateAPIView):
     serializer_class = LessonSerializer
     queryset = Lesson.objects.all()
-    permission_classes = [IsModerator]
+    permission_classes = [IsAuthenticated, IsModerator | IsOwner]
 
 
 class LessonDestroyAPIView(generics.DestroyAPIView):
     queryset = Lesson.objects.all()
+    permission_classes = [IsAuthenticated, IsOwner]
---
+++ users/management/commands/create_user.py
@@ -0,0 +1,26 @@
+from django.core.management import BaseCommand
+from users.models import User
+
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        user_name = 'user5'
+        user_email = f'{user_name}@sky.pro'
+        user_password = '123password'
+
+        if User.objects.filter(email=user_email).exists():
+            print(f'Пользователь с email {user_email} уже существует')
+            return
+
+        user, created = User.objects.get_or_create(
+            email=user_email,
+            defaults={
+                'first_name': user_name,
+                'is_superuser': False,
+                'is_staff': False,
+                'is_active': True,
+                'role': 'member'
+            }
+        )
+        user.set_password(user_password)
+        user.save()
