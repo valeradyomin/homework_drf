3ff9c091eb5d144a60da35eb80f5c4719d3062cf
Valera Demin <wahrwolffe@gmail.com>
2024-02-17 23:01:08 +0300

    HW4: add Subscription

--- materials/models.py
+++ materials/models.py
@@ -37,3 +37,15 @@ class Lesson(models.Model):
     class Meta:
         verbose_name = 'урок'
         verbose_name_plural = 'уроки'
+
+
+class Subscription(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, verbose_name='пользователь', **NULLABLE)
+    course = models.ForeignKey(Course, on_delete=models.CASCADE, verbose_name='курс', **NULLABLE)
+
+    def __str__(self):
+        return f'{self.user} - {self.course}'
+
+    class Meta:
+        verbose_name = 'подписка'
+        verbose_name_plural = 'подписки'
