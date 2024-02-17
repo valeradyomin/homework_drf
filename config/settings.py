b7b98134c6d17b372baaacf82d0a3a236e490cd0
Valera Demin <wahrwolffe@gmail.com>
2024-02-18 01:34:28 +0300

    HW4: set DEFAULT_PAGINATION_CLASS

--- config/settings.py
+++ config/settings.py
@@ -163,6 +163,8 @@ REST_FRAMEWORK = {
         # 'rest_framework.permissions.AllowAny',
         'rest_framework.permissions.IsAuthenticated',
     ],
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 11,
 }
 
 SIMPLE_JWT = {
